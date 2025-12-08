33bc7c3b80de0c2983a56f968dd1c2c8670e3ce7
Daniel Price <64694785+daniel-price@users.noreply.github.com>
2025-12-08 15:26:51 +0000

    cleanup aws

--- packages/aws/src/clients/lambda.ts
+++ packages/aws/src/clients/lambda.ts
@@ -3,26 +3,18 @@ import {
   FunctionConfiguration,
   GetFunctionCommand,
   GetFunctionCommandOutput,
-  GetFunctionConfigurationCommand,
   InvokeCommand,
   InvokeCommandOutput,
   LambdaClient,
   ListFunctionsCommand,
   UpdateFunctionCodeCommand,
   UpdateFunctionCodeCommandOutput,
-  UpdateFunctionConfigurationCommand,
 } from "@aws-sdk/client-lambda";
-import { ChangeItems, Json, Logger, R, retry } from "@dev/util";
+import { Json } from "@dev/util";
 import { confirmChangeItems } from "@dev/util/src/change-items";
 import fs from "fs";
 
-import { awsJSON, getQueryArg, yieldAll } from "../helpers/aws";
-
-const G_Lambda = R.Record({
-  FunctionName: R.String,
-});
-
-type T_Lambda = R.Static<typeof G_Lambda>;
+import { yieldAll } from "../helpers/aws";
 
 const lambda = new LambdaClient();
 
@@ -33,74 +25,6 @@ export function listLambdaFunctions(): AsyncGenerator<FunctionConfiguration> {
     );
     return { results: res.Functions, nextToken: res.NextMarker };
   });
-}
-
-export async function getLambdaFunctionNames(
-  queries?: string[],
-): Promise<string[]> {
-  const queryArg = getQueryArg(queries, "Functions", "FunctionName");
-  return await awsJSON(R.String, "lambda", "list-functions", queryArg);
-}
-
-export async function updateEnvVariable(
-  lambdaFn: T_Lambda,
-  varName: string,
-  varValue: string,
-  addIfNotExists = false,
-): Promise<void> {
-  const existingEnvVars = await getEnvironmentVariables(lambdaFn);
-  const existingValue = existingEnvVars[varName];
-  const shouldUpdate =
-    existingValue !== varValue &&
-    (existingValue !== undefined || addIfNotExists);
-
-  Logger.info(
-    `${lambdaFn} ${varName}="${existingValue}" -${
-      shouldUpdate ? "" : " not"
-    } updating to "${varValue}"`,
-  );
-
-  if (!shouldUpdate) return;
-
-  const newEnvVars = { ...existingEnvVars, [varName]: varValue };
-  const newEnv = { Variables: newEnvVars };
-
-  await lambda.send(
-    new UpdateFunctionConfigurationCommand({
-      FunctionName: lambdaFn.FunctionName,
-      Environment: newEnv,
-    }),
-  );
-}
-
-export async function getEnvironmentVariables(
-  lambdaFn: T_Lambda,
-): Promise<Record<string, string>> {
-  return retry(async () => {
-    const res = await lambda.send(
-      new GetFunctionConfigurationCommand({
-        FunctionName: lambdaFn.FunctionName,
-      }),
-    );
-
-    return res.Environment?.Variables || {};
-  });
-}
-
-export async function updateEnvVariables(
-  lambdaFn: T_Lambda[],
-  varName: string,
-  varValue: string,
-  addIfNotExists = false,
-): Promise<void> {
-  const update = (lambdaFn: T_Lambda): Promise<void> => {
-    return updateEnvVariable(lambdaFn, varName, varValue, addIfNotExists);
-  };
-  await ChangeItems.changeItems(
-    `update envVariable ${varName} to ${varValue}`,
-    lambdaFn,
-    update,
-  );
 }
 
 export async function invoke(
